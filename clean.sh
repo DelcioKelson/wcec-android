9599b04fc206e82618d7e3821809b6f87867bdb5
DelcioKelson <delciokelson@outlook.com>
2023-02-05 01:38:16 +0000

    clean dirs solved

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-find sootOutput/  -delete
-find files_to_analyse/  -delete
-rm cg.txt
-rm problem.lp
+find /tmp/sootOutput/  -delete
+find /tmp/files_to_analyse/  -delete
+rm /tmp/cg.txt
+rm tmp/problem.lp
 
